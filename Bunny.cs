79d41cd258b51be28cbb1eec8987d9f645daa86c
sglondon <susangs2@gmail.com>
2018-02-02 10:41:59 -0500

    intial commit

---
+++ Bunny.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Virtual_Pet
+{
+   public class Bunny
+    {
+
+        //create 4 fields in class Bunny
+
+        private string name;
+        private bool isHungry;
+        private int isBored;
+        private string health;
+
+        //create 4 properties
+
+    public string Name
+        {
+            get { return this.name; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
