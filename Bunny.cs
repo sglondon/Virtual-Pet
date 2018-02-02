679ef350e4fabc449371176efd5784454bdfc6cb
sglondon <susangs2@gmail.com>
2018-02-02 13:38:15 -0500

    working up thru option 3

--- Bunny.cs
+++ Bunny.cs
@@ -4,29 +4,109 @@ using System.Text;
 
 namespace Virtual_Pet
 {
-   public class Bunny
+    public class Bunny
     {
 
-        //create 4 fields in class Bunny
+        //Create 4 Fields in class Bunny
 
-        private string name;
+        private string name = "Twitch";
         private bool isHungry;
-        private int isBored;
+        private string isBored;
         private string health;
 
-        //create 4 properties
 
-    public string Name
+        //Create 4 Properties in class Bunny
+        //we're only going to use a get in the name, so
+        //property so the user can't change it
+
+        public string Name
         {
             get { return this.name; }
         }
 
 
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+
+
+        public string IsBored
+        {
+            get { return this.isBored; }
+            set { this.isBored = value; }
+        }
+
+        public string Health
+        {
+            get { return this.Health; }
+            set { this.Health = value; }
+        }
+
+        //Create Constructors
+
+        //default Constructor will also set our bunny's name
+
+        public Bunny()
+        {
+            this.name = "Twitch";
+        }
 
 
 
+        public Bunny(bool isHungry, string isBored, string health)
+        {
+
+            this.isHungry = isHungry;
+            this.isBored = isBored;
+            this.health = health;
+        }
 
 
+
+        //Create our Methods()
+
+        //method to display pet's name
+
+        public void BunnyName()
+        {
+
+            Console.WriteLine("Your Bunny's name is Twitch!");
+        }
+
+
+        //method to feed Twitch
+
+
+        public string FeedTwitch()
+        {
+            if (isHungry == true)
+            {
+                return "Twitch is hungry and needs to be fed!";
+            }
+            else
+            {
+                return "Twitch has been fed!";
+            }
+        }
+
+        public void  BoredBunny()
+        {
+            if (isBored == "1")
+            {
+                Console.WriteLine("Twitch is happy and alert!");
+            }
+            else if (isBored == "2")
+            {
+                Console.WriteLine("Twitch wants some attention!");
+            }
+            else if (isBored == "3")
+            {
+                Console.WriteLine("Twitch is very bored and is getting into mischief!");
+            }
+
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace Virtual_Pet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int userChoice;
+
+            Bunny userBunny = new Bunny(false, "3", "feels great");
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Hello! Let's meet your Virtual Pet!");
+                Console.WriteLine("Your Pet is a Bunny!");
+                Console.WriteLine("To see your pet's name, type 1");
+                Console.WriteLine("To see if your pet is hungry, type 2");
+                Console.WriteLine("To see if your pet needs some attention, type 3");
+                Console.WriteLine("To quit, type 8");
+
+                userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        userBunny.BunnyName(); 
+                        break;
+
+                    case 2:
+                        Console.WriteLine(userBunny.FeedTwitch());
+                        break;
+
+                    case 3:
+                        userBunny.BoredBunny();
+                        break;
+
+                }
+
+            } while (userChoice != 8);
+        }
         }
     }
-}
+
